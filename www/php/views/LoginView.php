8c1c2b760fc2140673edf18dbce001c16b7cb1ab
Matt <mfritze@ualberta.ca>
2014-10-04 10:04:38 -0600

    Added login button

--- www/php/views/LoginView.php
+++ www/php/views/LoginView.php
@@ -8,8 +8,10 @@
 	<body>
 		<?php $this->header->render(); ?>
 
-		<div id="content">
-			<a href="<?= $this->authURL;?>">Login!</a>
+		<div id="content" style='text-align:center'>
+			<a href="<?= $this->authURL;?>">
+				<button id='login' class='btn btn-success' type='button'> LOGIN </button> 
+			</a>
 		</div>
 
 		<?php $this->footer->render(); ?>
