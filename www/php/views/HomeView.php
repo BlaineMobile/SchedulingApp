5aa1f21bbec4cac38b33f45b0e496c2f6fc58ca3
Matt <mfritze@ualberta.ca>
2014-10-04 10:03:59 -0600

    Added link to bootstrap

--- www/php/views/HomeView.php
+++ www/php/views/HomeView.php
@@ -8,10 +8,17 @@
 	<body>
 		<?php $this->header->render(); ?>
 
-		<div id="content">
+		<div id="content" style='text-align:center'>
 
 			<h1>You're logged in</h1>
-			<a href="/create/new">Create new event</a>
+			<a href="/create/new">
+				<button id='create' class='btn btn-success' type='button'> CREATE </button>
+			</a>
+
+			<a href='#'>
+        		<button id='edit' class='btn btn-success' type='button'> EDIT </button>
+			</a>
+
 
 		</div>
 
