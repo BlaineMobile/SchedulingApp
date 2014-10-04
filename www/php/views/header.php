b4de144be653a869723308d990ec7330d41b246c
Matt <mfritze@ualberta.ca>
2014-10-04 10:05:39 -0600

    Added navbar

--- www/php/views/header.php
+++ www/php/views/header.php
@@ -1,3 +1,22 @@
 <div id="header">
-	
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">Scheduling App</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li>NAME HERE</li>
+            <li><a href="#">Logout</a></li> <!-- IF LOGGED IN -->
+            <li><a class='navbar-right' href="#">Settings</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 </div>
