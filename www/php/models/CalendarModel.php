97c8158352c813d3b33f51e2670be57126450740
twendlandt <twendlan@ualberta.ca>
2014-10-04 11:36:16 -0600

    added getAllCalendarList to CalendarModel

--- www/php/models/CalendarModel.php
+++ www/php/models/CalendarModel.php
@@ -2,13 +2,37 @@
 
 class CalendarModel {
 
+	const CALENDAR_NAME = "App Calendar";
+
 	private $user;
+	private $calendar;
 
 
 	public function __construct() {
 		require_once('Google/Service/Calendar.php');
 		$this->user = ModelFactory::getModel("UserModel");
+		getAllCalendarEvents();
 	}
+
+	// public function createCalendar() {
+	// 	$service = new Google_Service_
+	// 	$this->calendar = 
+	// }
+
+	public function getAllCalendarEvents() {
+		$service = new Google_CalendarService($this->user->getClient());
+		$eventsList = [];
+		$calList = $service->calendarList->listCalendarList();
+		foreach($calList as $cal) {
+			$eventsList = array_merge($eventsList, $service->events->listEvents());
+		}
+		print_r($eventsList);
+		return $eventsList;
+	}
+
+
+
+
 
 	// public function getTasksLists() {
 	// 	$service = new Google_Service_Tasks($this->user->getClient());
